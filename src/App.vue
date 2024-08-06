545a11cc8e20c0b9dae20e99084ee2a62cb1fd86
judilegend <judilegend2.com>
2024-08-06 21:42:57 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- Bouton avec état et texte conditionnels -->
+    <button :disabled="isButtonDisabled">
+      {{ isButtonDisabled ? "yes" : "non" }}
+    </button>
+
+    <!-- Bouton pour changer l'état de isButtonDisabled -->
+    <button @click="toggleButtonState">Toggle Button State</button>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  console.log(`the component is now mounted.`);
+});
+export default {
+  data() {
+    return {
+      isButtonDisabled: true, // Initialement désactivé
+    };
+  },
+  methods: {
+    toggleButtonState() {
+      // Inverse l'état de isButtonDisabled
+      this.isButtonDisabled = !this.isButtonDisabled;
+    },
+  },
+};
+</script>
---
+++ src/components/Binding.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app-2">
+    <span v-bind:title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const message = ref("You loaded this page on " + new Date().toLocaleString());
+</script>
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
